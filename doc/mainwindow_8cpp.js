3d763e94307479d8e0278725a929558fe5f50c41
Antonio Ruiz Ruiz <50016692+AntonioRuizR@users.noreply.github.com>
2024-07-04 09:47:17 +0200

    Add files via upload

---
+++ doc/mainwindow_8cpp.js
@@ -0,0 +1,4 @@
+var mainwindow_8cpp =
+[
+    [ "switchTranslator", "mainwindow_8cpp.html#afc81ad9bbdd28d8264c88749ef88b2ae", null ]
+];
---
+++ doc/navtreedata.js
@@ -0,0 +1,84 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "UCO-DWM-1001", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"class_q_c_p_abstract_paint_buffer.html#a3bc49cc9cf9daaca3a60977f010c08c9",
+"class_q_c_p_axis.html#add79d1e39c4ed65869a1e9cc79043f3f",
+"class_q_c_p_axis_ticker_pi.html#a262f1534c7f0c79a7d5237f5d1e2c54ca92f38a938c8b179b23363d9993681c55",
+"class_q_c_p_color_map_data.html#a225bb96f10c1a27b51ae59249477dbef",
+"class_q_c_p_error_bars.html#a7cba420078adc523efa59fb8c6ca23e0",
+"class_q_c_p_item_curve.html#a28181a9dee9cc3c3da83a883221bd2d0",
+"class_q_c_p_item_text.html#a77ff96a2972a00872ff8f8c67143abbe",
+"class_q_c_p_layout_element.html#a2fa26d3d22a2854c4da1d80f188db961",
+"class_q_c_p_painter.html#a8fd6821ee6fecbfa04444c9062912abd",
+"class_q_c_p_polar_axis_radial.html#a010c5af35d5638d16ba4f71a749c0bce",
+"class_q_c_p_polar_graph.html#ab69bb9f6074ba180e74e535fd6494f96",
+"class_q_c_p_selection_rect.html#af67bc58f4f5ce9a4dc420b9c42de235a",
+"class_q_custom_plot.html#aa27569c92e74395af10151357d268628",
+"namespace_q_c_p.html#a5400e5fcb9528d92002ddb938c1f4ef4aa5fd227bc878c56ad2a87ea32c74ee4d"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
