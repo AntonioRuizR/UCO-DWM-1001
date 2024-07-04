d36497992e81b5721ebcf546e673133125d1be74
Antonio Ruiz Ruiz <50016692+AntonioRuizR@users.noreply.github.com>
2024-07-04 09:37:23 +0200

    Add files via upload

---
+++ doc/class_dialog__mediafilter.js
@@ -0,0 +1,11 @@
+var class_dialog__mediafilter =
+[
+    [ "Dialog_mediafilter", "class_dialog__mediafilter.html#aa30909f2a773f0e937d5b404a3eaf4fc", null ],
+    [ "~Dialog_mediafilter", "class_dialog__mediafilter.html#a9db72fba71fb2f55181b89f466fa4f70", null ],
+    [ "bufer_data", "class_dialog__mediafilter.html#a2adb713089fcbe793ac4603f12e5c397", null ],
+    [ "enable_mediafilter", "class_dialog__mediafilter.html#ada3242e9dbd4b69023f4eaa929bd6320", null ],
+    [ "link_alarm", "class_dialog__mediafilter.html#ae9b9ede0ac2bad05b8518b7d49034105", null ],
+    [ "load_data_bufer", "class_dialog__mediafilter.html#a3dda6a5bffd3a740747aaf62570a0371", null ],
+    [ "alarm_linked", "class_dialog__mediafilter.html#ab2c5507b6da692881b47bb1527d6417d", null ],
+    [ "media_enabled", "class_dialog__mediafilter.html#a97678603833bc541012c5fa170fcf2fc", null ]
+];
